887024a45ce81feebf3ccbdf128ce456e73a2060
SoloLeveling019 <adm.josed@gmail.com>
2026-07-29 05:38:17 -0500

    Delete legacy directory

--- legacy/glaucoma_notice.py
+++
@@ -1,8 +0,0 @@
-def get_glaucoma_notice() -> str:
-    return (
-        "El glaucoma no puede detectarse de forma confiable mediante una "
-        "fotografía externa convencional. Requiere evaluación oftalmológica, "
-        "presión intraocular y fondo de ojo. El sistema solo puede advertir "
-        "signos externos compatibles en casos agudos, como ojo muy rojo, "
-        "pupila fija o córnea opaca."
-    )
